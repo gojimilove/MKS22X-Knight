429273263e2844ac4b170f17730ce0a10ac31810
Jessica Kim <jkim00@stuy.edu>
2019-02-12 19:43:23 -0500

    private instance variables and constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,3 +1,11 @@
 public class KnightBoard {
-  
+  private int[][] board,moves;
+
+  /**
+  *@throws IllegalArgumentException when either parameter is negative
+  */
+  public KnightBoard(int startingRows, int startingCols) {
+  	board = new int[startingRows][startingCols];
+  	moves = new int[startingRows][startingCols];
+  }
 }
