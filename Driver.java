83fdc7e5fd06f60a21d897125299ef42c45468a9
Jessica Kim <jkim00@stuy.edu>
2019-02-12 20:18:50 -0500

    printing out board and moves

--- Driver.java
+++ Driver.java
@@ -1,5 +1,7 @@
 public class Driver {
 	public static void main(String[] args) {
-		
+		KnightBoard test = new KnightBoard(2,2);
+		System.out.println(test);
+		System.out.println(test.printMoves());
 	}
 }
