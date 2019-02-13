6956c457102bc4ed44df35dd8a8292595e339a1c
Jessica Kim <jkim00@stuy.edu>
2019-02-12 19:55:33 -0500

    toString works

--- KnightBoard.java
+++ KnightBoard.java
@@ -6,11 +6,26 @@ public class KnightBoard {
   */
   public KnightBoard(int startingRows, int startingCols) {
   	board = new int[startingRows][startingCols];
+  	int num = 1;
+  	for (int i = 0; i < board.length; i++) {
+  		for (int j = 0; j < board[0].length; j++) {
+  			board[i][j] = num;
+  			num++;
+  		}
+  	}
   	moves = new int[startingRows][startingCols];
   }
 
   public String toString() {
-
+  	String s = "";
+  	for (int i = 0; i < board.length; i++) {
+  		for (int j = 0; j < board[0].length; j++) {
+  			if (board[i][j] < 10) s = s + " " + board[i][j] + " ";
+  			else s += board[i][j] + " ";
+  		}
+  		s+="\n";
+  	}
+  	return s;
   }
 
   /**
@@ -18,7 +33,7 @@ public class KnightBoard {
 	*@throws IllegalArgumentException when either parameter is negative or out of bounds.
 	*/
 	public boolean solve(int startingRow, int startingCol) {
-
+		return false;
 	}
 
 	/**
@@ -26,6 +41,6 @@ public class KnightBoard {
 	*@throws IllegalArgumentException when either parameter is negative or out of bounds.
 	*/
 	public int countSolutions(int startingRow, int startingCol) {
-		
+		return 0;
 	}
 }
