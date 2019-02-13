1bd38ec6482d91bc8447614954bd316669597a79
Jessica Kim <jkim00@stuy.edu>
2019-02-12 19:45:05 -0500

    empty methods

--- KnightBoard.java
+++ KnightBoard.java
@@ -8,4 +8,24 @@ public class KnightBoard {
   	board = new int[startingRows][startingCols];
   	moves = new int[startingRows][startingCols];
   }
+
+  public String toString() {
+
+  }
+
+  /**
+  *@throws IllegalStateException when the board contains non-zero values.
+	*@throws IllegalArgumentException when either parameter is negative or out of bounds.
+	*/
+	public boolean solve(int startingRow, int startingCol) {
+
+	}
+
+	/**
+  *@throws IllegalStateException when the board contains non-zero values.
+	*@throws IllegalArgumentException when either parameter is negative or out of bounds.
+	*/
+	public int countSolutions(int startingRow, int startingCol) {
+		
+	}
 }
