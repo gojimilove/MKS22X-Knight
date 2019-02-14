a9c2130e005694384604589d5dca701dd50eba04
Jessica Kim <jkim00@stuy.edu>
2019-02-14 11:55:39 -0500

    testing add and remove

--- Driver.java
+++ Driver.java
@@ -1,7 +1,11 @@
 public class Driver {
 	public static void main(String[] args) {
-		KnightBoard test = new KnightBoard(2,2);
+		KnightBoard test = new KnightBoard(5,5);
 		System.out.println(test);
-		System.out.println(test.printMoves());
+		test.addKnight(2,1);
+		System.out.println(test);
+		test.removeKnight(2,1);
+		System.out.println(test);
+		//System.out.println(test.printMoves());
 	}
 }
