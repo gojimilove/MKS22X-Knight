4210943d20d83db57878a41b8faf2baf38332981
Jessica Kim <jkim00@stuy.edu>
2019-02-27 02:43:37 -0500

    illegal argument exceptions for count, solve, and constructor, also added documentation

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,6 +7,10 @@ public class KnightBoard {
   *@throws IllegalArgumentException when either parameter is negative
   */
   public KnightBoard(int startingRows, int startingCols) {
+  	//if either parameter is negative
+  	if (startingRows < 0 || startingCols < 0) {
+  		throw new IllegalArgumentException("parameters can't be negative");
+  	}
   	board = new int[startingRows][startingCols];
   	//int num = 1;
   	for (int i = 0; i < board.length; i++) {
@@ -17,9 +21,9 @@ public class KnightBoard {
   		}
   	}
   	moves = new int[startingRows][startingCols];
-    fillMoves();
-    rowMove = new int[]{-2,-2,-1,1,2,2,-1,1};
-    colMove = new int[]{-1,1,2,2,-1,1,-2,-2};
+    fillMoves(); //fills the new board with how many moves are possible from each square
+    rowMove = new int[]{-2,-2,-1,1,2,2,-1,1}; //row component of all 8 moves
+    colMove = new int[]{-1,1,2,2,-1,1,-2,-2}; //col component of all 8 moves
     counter = 0;
   }
 
@@ -29,7 +33,7 @@ public class KnightBoard {
 	  		for (int j = 0; j < moves[0].length; j++) {
 	  			if (i < 2 || i > moves.length-3 || j < 2 || j > moves[0].length-3) {
 	  				if ((i == 0 || i == moves.length-1) && (j == 0 || j == moves[0].length-1)) {
-	  					moves[i][j] = 2;
+	  					moves[i][j] = 2; //corners
 	  				} else if ((i == 1 && j == 0) ||
 	  									 (i == 1 && j == moves[0].length-1) ||
 	  									 (i == 0 && j == 1) ||
@@ -38,7 +42,7 @@ public class KnightBoard {
 	  									 (i == moves.length-2 && j == moves[0].length-1) ||
 	  									 (i == moves.length-1 && j == 1) ||
 	  									 (i == moves.length-1 && j == moves[0].length-2)) {
-	  					moves[i][j] = 3;
+	  					moves[i][j] = 3; //next layer
 	  				} else if (i == 0 ||
 	  									i == moves.length-1 ||
 	  									j == 0 ||
@@ -47,18 +51,19 @@ public class KnightBoard {
 	  									(i == 1 && j == moves[0].length-2) ||
 	  									(i == moves.length-2 && j == 1) ||
 	  									(i == moves.length-2 && j == moves[0].length-2)) {
-	  					moves[i][j] = 4;
+	  					moves[i][j] = 4; //next layer
 	  				} else if (i == 1 || i == moves.length-2 || j == 1 || j == moves[0].length-2){
-	  					moves[i][j] = 6;
+	  					moves[i][j] = 6; //middle edges
 	  				}
 	  			}
-	  			else moves[i][j] = 8;
+	  			else moves[i][j] = 8; //middle spaces
 	  		}
 	  	}
 	  }
   }
 
   public boolean addKnight(int r, int c) {
+    //if out of bounds return false
     if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != 0) return false;
     else {
       board[r][c] = 1;
@@ -67,6 +72,7 @@ public class KnightBoard {
   }
 
   public boolean removeKnight(int r, int c) {
+  	//if out of bounds return false
     if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] == 0) return false;
     else {
       board[r][c] = 0;
@@ -92,6 +98,9 @@ public class KnightBoard {
   	return s;
   }
 
+  /**
+  *@return properly formatted string of the moves board
+  */
   public String printMoves() {
   	String s = "";
   	for (int i = 0; i < moves.length; i++) {
@@ -116,18 +125,24 @@ public class KnightBoard {
   			if (board[i][j] != 0) throw new IllegalStateException("the board is not empty");
   		}
   	}
+  	//either parameter is negative or out of bounds
+  	if (startingRow < 0 || startingRow >= board.length || 
+  			startingCol < 0 || startingCol >= board[0].length) {
+  		throw new IllegalArgumentException("starting parameters are out of bounds");
+  	}
+  	//initial knight placed at starting position
   	addKnight(startingRow, startingCol);
 		return solveH(startingRow, startingCol, 2);
 	}
 
   private boolean solveH(int row, int col, int count) {
-    if (count > board.length * board[0].length) {
+    if (count > board.length * board[0].length) { //base case: when all spaces on the board have a knight
     	//System.out.println(toString());
       return true;
     }
     else {
     	for (int i = 0; i < 8; i++) {
-    		if (addKnight(row+rowMove[i], col+colMove[i])) {
+    		if (addKnight(row+rowMove[i], col+colMove[i])) { //check the 8 moves see if they're valid
     			//System.out.println(toString());
     			//System.out.println(count);
     			if (solveH(row+rowMove[i],col+colMove[i],count+1)) return true;
@@ -145,24 +160,32 @@ public class KnightBoard {
 	*/
 	public int countSolutions(int startingRow, int startingCol) {
 		//if the board starts with non-zero values throw exception
-  	for (int i = 0; i < board.length; i++) {
+  	for (int i = 0; i < board.length; i++) { //base case: when all spaces on the board have a knight
   		for (int j = 0; j < board[0].length; j++) {
   			if (board[i][j] != 0) throw new IllegalStateException("the board is not empty");
   		}
   	}
+  	//either parameter is negative or out of bounds
+  	if (startingRow < 0 || startingRow >= board.length || 
+  			startingCol < 0 || startingCol >= board[0].length) {
+  		throw new IllegalArgumentException("starting parameters are out of bounds");
+  	}
+  	//initial knight placed at starting position
   	addKnight(startingRow, startingCol);
+  	//find solutions, add up counter
 		countH(startingRow, startingCol, 2);
+		//clear board (remove initial knight)
 		removeKnight(startingRow, startingCol);
 		return counter;
 	}
 
   private boolean countH(int row, int col, int count) {
     if (count > board.length * board[0].length) {
-    	counter++;
+    	counter++; //if a solution is found add it up
     }
     else {
     	for (int i = 0; i < 8; i++) {
-    		if (addKnight(row+rowMove[i], col+colMove[i])) {
+    		if (addKnight(row+rowMove[i], col+colMove[i])) { //check the 8 moves see if they're valid
     			//System.out.println(toString());
     			//System.out.println(count);
     			if (countH(row+rowMove[i],col+colMove[i],count+1)) return true;
