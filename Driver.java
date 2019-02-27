c1c8f006fc8ec030def06661eea69365ec59d5be
Jessica Kim <jkim00@stuy.edu>
2019-02-27 02:44:06 -0500

    added a couple tests for solve and count

--- Driver.java
+++ Driver.java
@@ -1,8 +1,12 @@
 public class Driver {
 	public static void main(String[] args) {
 		KnightBoard test = new KnightBoard(5,5);
+		KnightBoard test2 = new KnightBoard(5,5);
+		//System.out.println(test);
+		System.out.println(test.solve(0,0));
+		System.out.println(test2.countSolutions(0,0));
 		System.out.println(test);
-		System.out.println(test.solve(2,2));
+		System.out.println(test2);
 		// test.addKnight(2,1);
 		// System.out.println(test);
 		// test.removeKnight(2,1);
