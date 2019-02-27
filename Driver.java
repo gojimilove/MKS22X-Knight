5c7b22c00754791c2816500fdf5c661d25230602
Jessica Kim <jkim00@stuy.edu>
2019-02-27 01:31:28 -0500

    solve works on 5x5 board starting from anywhere, tested

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver {
 	public static void main(String[] args) {
 		KnightBoard test = new KnightBoard(5,5);
 		System.out.println(test);
-		System.out.println(test.solve(0,0));
+		System.out.println(test.solve(2,2));
 		// test.addKnight(2,1);
 		// System.out.println(test);
 		// test.removeKnight(2,1);
